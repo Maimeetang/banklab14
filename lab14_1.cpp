931c3bb42fabcbb007e01b1c395ef0527396c59a
Bank <sirawit.36298@gmail.com>
2023-01-13 15:45:13 +0700

    lab14_1 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,30 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double number[],int N,double sum[]){
+    double sumx=0;
+    double sum2x=0;
+    double sum3x=1;
+    double sum4x=0;
+    double max=number[0];
+    double min=number[0];
+    for(int i=0;i<N;i++){
+        sumx += number[i];
+        sum2x += pow(number[i],2);
+        sum3x *= number[i];
+        sum4x += 1.0/number[i];
+        if(max<number[i]){
+            max = number[i];
+        }
+        if(min>number[i]){
+            min = number[i];
+        }
+    }
+    sum[0]=sumx/N;
+    sum[1]=sqrt((sum2x/N)-pow(sum[0],2));
+    sum[2]=pow(sum3x,1.0/N);
+    sum[3]=N/sum4x;
+    sum[4]=max;
+    sum[5]=min;
+}
